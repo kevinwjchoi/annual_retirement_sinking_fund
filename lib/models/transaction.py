acb5c69f18779e34d14d5006c871d1277992be1a
Kevin Choi <kevinnwjchoi@gmail.com>
2024-05-26 18:30:22 -0400

    transaction.py init, repr

--- lib/models/transaction.py
+++ lib/models/transaction.py
@@ -1,5 +1,15 @@
 from models.__init__ import CURSOR, CONN
-from lib.models.account import SinkingFund
+from lib.models.account import Account
 
 class Transaction:
-    pass
+    
+    def __init__(self, transaction_id, account_id, amount, transaction_type, timestamp):
+        self.transaction_id = transaction_id
+        self.account_id = account_id
+        self.amount = amount
+        self.transaction_type = transaction_type
+        self.timestamp = timestamp
+
+    def __repr__(self):
+        return (f"Transaction({self.transaction_id}, {self.account_id}, {self.amount}, "
+                f"{self.transaction_type}, {self.timestamp})")
