f0af87af3511210fc479283d929112f28e7462e8
Kevin Choi <kevinnwjchoi@gmail.com>
2024-05-27 16:20:35 -0400

    department.py creating class method create_table

--- lib/models/account.py
+++ lib/models/account.py
@@ -9,7 +9,38 @@ class Account:
         self.taxed = taxed
         self.date_created = date_created if date_created else datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.goal = goal
-        
+
     def __repr__(self):
         return (f"Account({self.account_id}, {self.name}, {self.balance}, {self.taxed}, "
                 f"{self.date_created}, {self.goal})")
+
+
+    
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError(
+                "Name must be a non-empty string"
+            )
+
+    @classmethod
+    def create_table(cls):
+        """ Create a new table to persist the attributes of Account instances """
+        sql = """
+            CREATE TABLE IF NOT EXISTS accounts (
+            account_id INTEGER PRIMARY KEY,
+            name TEXT,
+            balance REAL,
+            taxed INTEGER,
+            date_created TEXT,
+            goal REAL
+            )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
